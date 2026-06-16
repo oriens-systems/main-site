0fcb587fefd0a5882b8643e3183b3b026c541387
jaffer <75342062+J-992@users.noreply.github.com>
2026-06-15 22:40:58 -0400

    cm

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -109,7 +109,7 @@ export default function Footer() {
                     href="mailto:hello@orienssystems.com"
                     className="text-[13px] text-white/40 hover:text-white/80 transition-colors duration-200"
                   >
-                    hello@orienssystems.com
+                    hello@oriens.systems
                   </a>
                 </li>
                 <li key="contact-location">
